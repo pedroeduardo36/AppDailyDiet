c12bd36c1a70a081a86459f35ab57f599db53466
Pedro <pedrovilarinho.gpd@gmail.com>
2022-12-16 17:54:18 -0300

    feat @types theme

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,13 +2,13 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
-    background-color: #333;
+    background-color: ${({theme}) => theme.COLORS.WHITE};
     align-items: center;
     justify-content: center;
 `
 
 export const Text = styled.Text`
-    font-size: 32px;
-    color: #FFFF;
+    font-size: ${({theme}) => theme.FONT_SIZE.LG}px;
+    color: ${({theme}) => theme.COLORS.GRAY_600};
     font-weight: 900;
 `
